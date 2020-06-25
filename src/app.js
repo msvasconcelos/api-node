8b3021924afcd0c895bc30e3d2b70b15b7368a94
Matheus Vasconcelos <matheus-s.v@hotmail.com>
2020-06-25 11:32:37 -0300

    add connection with mongoDB

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 // App
 const app = express();
+// Database
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useCreateIndex: true 
+});
+
+const db = mongoose.connection;
+  
+db.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+db.on('error', err => {
+    console.log(`Mongoose default connection has occured \n${err}`);
+});
+
+db.on('disconnected', () => {
+    console.log('Mongoose default connection is disconnected');
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log(
+        'Mongoose default connection is disconnected due to application termination'
+        );
+        process.exit(0);
+    });
+});
 
 // Load routes
 const indexRoutes = require('./routes/index-routes');
